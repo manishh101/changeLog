2e17e859e35865903f88b07088922f8da504b7fb
git config --global user.email "$github_email <echo "Git configuration updated>
2025-06-25 17:19:01 +0545

    Add express-validator for input validation and implement middleware for error handling

--- api/changelog/src/router.ts
+++ api/changelog/src/router.ts
@@ -1,4 +1,6 @@
 import {Router} from 'express'
+import { body, oneOf, validationResult } from "express-validator";
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router()
 
@@ -12,29 +14,54 @@ router.get('/product', (req,res) => {
 router.get('/product/:id', (req,res)=> {
   res.json({message: 'message'})
 })
-router.put('/product/:id', (req,res)=> {})
-router.post('/product', (req,res)=> {})
+router.put('/product/:id', body('name').isString(),handleInputErrors, (req,res)=> {
+  
+})
+router.post('/product',body('name').isString(),handleInputErrors,  (req,res)=> {})
 router.delete('/product/:id', (req,res)=> {})
 
 /**
  * Update
  * */ 
 
-router.get('/update', ()=>{})
-router.get('/update/:id', ()=> {})
-router.put('/update/:id', ()=> {})
-router.post('/update', ()=> {})
-router.delete('/update/:id', ()=> {})
+router.get('/update', (req,res)=>{})
+router.get('/update/:id', (req,res)=> {})
+router.put('/update/:id',
+  body('title').optional(), 
+  body('body').optional(),
+  oneOf([
+    body('status').equals('IN_PROGRESS'),
+    body('status').equals('SHIPPED'),
+    body('status').equals('DEPRECATED')
+  ]), 
+  body('version').optional(),
+  body('asset').optional() ,  (req,res)=> {
+
+  })
+router.post('/update',
+  body('title').exists(), 
+  body('body').exists(),
+  body('productId').exists().isString(), (req,res)=> {
+
+  })
+router.delete('/update/:id', (req,res)=> {})
 
 /**
  * Update point
  */
 
-router.get('/updatepoint', ()=>{})
-router.get('/updatepoint/:id', ()=> {})
-router.put('/updatepoint/:id', ()=> {})
-router.post('/updatepoint', ()=> {})
-router.delete('/updatepoint/:id', ()=> {})
+router.get('/updatepoint', (req,res)=>{})
+router.get('/updatepoint/:id', (req,res)=> {})
+router.put('/updatepoint/:id',
+  body('name').optional().isString(),
+  body('description').optional().isString() ,(req,res)=> {
+
+  })
+router.post('/updatepoint',
+  body('name').exists().isString(),
+  body('description').exists().isString(),
+  body('updateId').exists().isString() ,(req,res)=> {})
+router.delete('/updatepoint/:id', (req,res)=> {})
 
 
 export default router
